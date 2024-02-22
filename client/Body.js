6941953d15c9da3442730fcc53ab6d7692d4e071
ankitavashisth02 <ankitavashisth02@gmail.com>
2024-02-22 12:03:10 +0530

    22/feb

--- client/Body.js
+++ client/Body.js
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
 import nftImg from "./assets/NFT-Img.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { ethers, parseEther } from "ethers";
 
 const Body = ({ state }) => {
   const { contract } = state;
- 
 
-  const onHandleChange = async()=>{
+  const onHandleChange = async () => {
+    const amount = { value: parseEther("0.000000000001") };
+    console.log("Amount:", amount.value.toString());
+    const tokenId = await contract._tokenIds();
     
-        const amount = {value : parseEther("0.000000000001")};
-        console.log("Amount:",amount.value.toString());
-        const tokenId = await contract._tokenIds();
-        //await tokenId.wait();
-        console.log("token Id:", tokenId);
-        const burned = await contract.burn1(parseInt(tokenId), parseInt(amount.value.toString()));
-        await burned.wait();
-        console.log('Burned');
-   
-  }
+    console.log("token Id:", tokenId);
+    const burned = await contract.burn1(
+      parseInt(tokenId),
+      parseInt(amount.value.toString())
+    );
+    await burned.wait();
+    toast("nft Burned !!");
+    console.log("Burned");
+  };
 
   async function mintNFT(tokenURI) {
     try {
-      console.log(tokenURI);
-
+      //console.log(tokenURI);
       const options = { value: parseEther("0.01") };
-
       const txn = await contract.mintNFT(tokenURI, options);
-      await txn.wait();
+      
+      await txn.wait() ? toast("Minted"): toast("Error in minting !!");
+
       console.log("Minted NFT!");
     } catch (error) {
       console.log(error);
@@ -46,9 +49,8 @@ const Body = ({ state }) => {
         Mint
       </button>
 
-      <button onClick={() =>onHandleChange()}>
-        Burn
-      </button>
+      <button onClick={() => onHandleChange()}>Burn</button>
+      <ToastContainer/>
     </div>
   );
 };
