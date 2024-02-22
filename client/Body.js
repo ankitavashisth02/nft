084c8d6a2eafbde15678f30b814f37686041ebc2
ankitavashisth02 <ankitavashisth02@gmail.com>
2024-02-22 13:55:25 +0530

    22-feb/final/1

--- client/Body.js
+++ client/Body.js
@@ -6,13 +6,18 @@ import { ethers, parseEther } from "ethers";
 
 const Body = ({ state }) => {
   const { contract } = state;
+  const[loading, setLoading] = useState(false);
+  const[burning, setBurning] = useState(false);
 
   const onHandleChange = async () => {
     const tokenId = await contract._tokenIds();
-    const burnedAmount = ((Number(tokenId)-1)**2)/16000;
+    const burnedAmount = (((Number(tokenId))-1)**2)/16000;
     const amt = burnedAmount.toString();
-    const amount = { value: parseEther(amt) };
+
+    const amount = { value: parseEther("0.0000001") };
     console.log("Amount:", amount.value.toString());
+
+    setBurning(true);
     
     const burned = await contract.burn1(
       parseInt(tokenId),
@@ -20,6 +25,8 @@ const Body = ({ state }) => {
     );
 
     await burned.wait();
+    setBurning(false);
+
     toast("nft Burned !!");
     console.log("Burned");
   };
@@ -31,11 +38,15 @@ const Body = ({ state }) => {
       const mintingAmount = (Number(tokenId)**2)/16000;
       const amt = (mintingAmount).toString();
       const options = { value: parseEther(amt) };
-      const txn = await contract.mintNFT(tokenURI, options);
-      
-      await txn.wait() ? toast("Minted"): toast("Error in minting !!");
 
-      console.log("Minted NFT!");
+      setLoading(true);
+
+      const txn = await contract.mintNFT(tokenURI, options);
+      await txn.wait() ; 
+      setLoading(false);
+
+      toast("Minted");
+      //console.log("Minted NFT!");
     } catch (error) {
       console.log(error);
     }
@@ -44,17 +55,17 @@ const Body = ({ state }) => {
   return (
     <div className="center-container">
       <img src={nftImg} />
-      <button
-        onClick={() =>
+      <button onClick={() =>
           mintNFT(
             "https://gateway.pinata.cloud/ipfs/QmbmoyEnwjArC8JXhDU4zQcoPq2FdvjzeoJHZTBjRBsBGg"
           )
-        }
-      >
-        Mint
+        }>
+        {loading ? "Minting.." : "Mint"}
       </button>
 
-      <button onClick={() => onHandleChange()}>Burn</button>
+      <button onClick={() => onHandleChange()}>
+        {burning ? "Burning.." : "Burn"}
+      </button>
       <ToastContainer/>
     </div>
   );
